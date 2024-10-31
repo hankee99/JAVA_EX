8d189aaa36b450a1c533a2d26737c8d086bb6e8d
hankee99 <hankee60@gmail.com>
2024-10-31 23:03:35 +0900

    Initial commit

---
+++ Programmers/src/level0/Level0_abPrint.java
@@ -0,0 +1,22 @@
+package level0;
+import java.util.Scanner;
+/*입출력 예
+입력 #1
+4 5
+
+출력 #1
+a = 4
+b = 5
+*/
+public class Level0_abPrint {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+
+	}
+
+}
